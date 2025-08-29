a6cd0ed06651a58900ace61492bcb3826a0e2a09
HazGri <hazemgherissi@gmail.com>
2025-08-29 20:22:07 +0200

    fix

--- app/composants/Projects.jsx
+++ app/composants/Projects.jsx
@@ -88,14 +88,12 @@ export function Projects() {
   ]);
 
   const [isLinkDisabled, setIsLinkDisabled] = useState(false);
-
   const handleLinkClick = (e, link) => {
     e.preventDefault();
-
     setIsLinkDisabled(true);
 
     setTimeout(() => {
-      window.location.href = link;
+      window.open(link, "_blank", "noopener,noreferrer");
     }, 500);
   };
 
@@ -111,6 +109,8 @@ export function Projects() {
               <CardContent className="relative p-0 h-full w-fit cursor-pointer">
                 <a
                   href={isLinkDisabled ? "#" : project.link} // Lien désactivé si isLinkDisabled
+                  target="_blank"
+                  rel="noopener noreferrer"
                   onClick={(e) => handleLinkClick(e, project.link)}
                   className="focus:outline-none focus:ring-0" // Gère le clic
                 >
